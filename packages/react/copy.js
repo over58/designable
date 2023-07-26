4baf5dd0a905e243fe4add42f3429cf4dbee092c
xuyongchao@bytedance.com <xuyongchao@bytedance.com>
2023-07-26 22:57:22 +0800

    feat: copy

--- packages/react/copy.js
+++ packages/react/copy.js
@@ -1,5 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { runCopy } = require('../../scripts/build-style/copy')
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-var-requires 
+const { runCopy } = require('../../scripts/build-style/copy.js')
 
 runCopy({
   esStr: 'antd/es/',
